2b951784a8fe8293d189fc7fed183200c240c52c
JustinLGates <papagates1@gmail.com>
2020-07-05 22:44:48 -0600

    added map room and user

--- game/game.cs
+++ game/game.cs
@@ -1,9 +1,15 @@
 using System;
 using game.models;
+using System.Collections.Generic;
 namespace game.run
 {
   class Game
   {
+    //this is the only map 
+    Map map = Map.Instance;
+    Room currentRoom;
+    Vector2 playerLocation = new Vector2(0, 0);
+    // Room currentRoom;
     bool running = false;
     public Game()
     {
@@ -28,25 +34,27 @@ namespace game.run
       }
       running = true;
       Console.Clear();
-      Console.WriteLine($"nice to meet you {userName}");
-      Console.WriteLine($"your journy starts here in the forest...");
-      Console.WriteLine("press any key to continue...");
+
+      Console.WriteLine($"ah yes, {userName} i remember you.");
+      Console.WriteLine($"your journey starts here in the forest of doom. I hear its lovely this time of year.");
+      Console.WriteLine("press enter to continue...");
       Console.ReadLine();
-      Console.WriteLine("ah the forest a great place to get lost.....");
       while (running)
       {
         Console.Clear();
         Console.WriteLine(@"
-      -------------------------------------------------------------------------------
-        (lookaround)={l}  (move)={n,s,e,w}  (pickup) {p + item name}  (quit) {q}
+     
+             (lookaround)={l}  (move)={m}  (pickup) {p + item name}  (quit) {q}
       ------------------------------------------------------------------------------- ");
         string userInput = Console.ReadLine();
 
         switch (userInput)
         {
           case "l":
+            description();
             break;
-          case "move":
+          case "m":
+            move();
             break;
           case "p":
 
@@ -58,14 +66,72 @@ namespace game.run
             Console.Clear();
             Console.WriteLine("that was not a valid option try one of theese commands");
             Console.WriteLine(@"
-      -------------------------------------------------------------------------------
-        (lookaround)={l}       (move)={n,s,e,w}      (pickup) {itemname}
+    
+              (lookaround)={l}       (move)={n,s,e,w}      (pickup) {itemname}
       ------------------------------------------------------------------------------- ");
             break;
         }
       }
-      Console.WriteLine("you died because you are a quiter...");
+      Console.WriteLine("you died...");
     }
+
+    void description()
+    {
+      Console.Clear();
+      Console.WriteLine("this area is coverd in sand there is not much here. It looks like we can go west or north...");
+      Console.WriteLine(playerLocation.x + " " + playerLocation.y);
+
+
+
+      Console.WriteLine(@"
+        (move)={m}    (pickup) {p + item name}    (exit) {}
+      -------------------------------------------------------");
+      string userInput = Console.ReadLine();
+      switch (userInput)
+      {
+        case "m":
+          move();
+          break;
+      }
+
+    }
+    void move()
+    {
+      Console.Clear();
+      bool validInput = false;
+      Console.WriteLine(@"
+                              Where would you like to go?
+                --------------------------------------------------------
+                (north){n}  (south){s}  (east){e}  (west){w} (cancel) {}");
+      while (!validInput)
+      {
+
+        string userInput = Console.ReadLine();
+        switch (userInput)
+        {
+          case "n":
+            validInput = true;
+            break;
+          case "e":
+            break;
+          case "s":
+            break;
+          case "w":
+            break;
+        }
+        Console.Clear();
+        Console.WriteLine(@"
+                      that is not an option try again. 
+         --------------------------------------------------------
+         (north){n}  (south){s}  (east){e}  (west){w} (cancel) {}");
+      }
+
+    }
+
+
+
+
+
   }
 
 };
